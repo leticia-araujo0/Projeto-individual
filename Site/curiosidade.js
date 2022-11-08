fb82aa626905996f0456e5fa3f7ab4d479a897e0
leticia-araujo0 <leticia.araujo@sptech,school>
2022-11-08 00:06:46 -0300

    Att tela de login, correção tela de cadastro e add alerts de curiosidade

--- Site/curiosidade.js
+++ Site/curiosidade.js
@@ -1,1 +1,63 @@
-// fazr o rolê dos alert aqui
+// fazr o rolê dos alert aqui 
+
+function curiosidade() {
+
+    var sorteio = parseInt(1 + Math.random() * 3)
+    
+if (sorteio == 1){
+    alert (`aaadeu certo`)
+}
+
+if (sorteio == 2){
+    alert (`bbbbdeu certo`)
+}
+if (sorteio == 3){
+    alert (`ccccdeu certo`)
+}
+}
+if (sorteio == 4){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 5){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 6){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 7){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 8){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 9){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 10){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 11){
+    alert (`ccccdeu certo`)
+
+}
+if (sorteio == 12){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 13){
+    alert (`ccccdeu certo`)
+}
+
+if (sorteio == 14){
+    alert (`ccccdeu certo`)
+}
+if (sorteio == 15){
+    alert (`ccccdeu certo`)
+}
