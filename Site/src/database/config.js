c92cea39f5f4bcea1a0c9d3651b1a07e4ee735ae
leticia-araujo0 <leticia.araujo@sptech,school>
2022-11-23 00:36:07 -0300

    Conexão do login ao banco local

--- Site/src/database/config.js
+++ Site/src/database/config.js
@@ -20,7 +20,7 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "aquatech",
+    database: "Melodoramatica",
     user: "root",
     password: "xrm847frito",
 };
